c4f4c68f6f0cfda61877c870ddf57782346dc0db
naman8586 <namanverma8586@gmail.com>
2026-02-09 14:01:55 +0530

    updated app

--- apps/web/lib/prisma.js
+++ apps/web/lib/prisma.js
@@ -1,14 +1,14 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from "@prisma/client";
 
-const globalForPrisma = global;
+const globalForPrisma = globalThis;
 
 export const prisma =
   globalForPrisma.prisma ||
   new PrismaClient({
-    log: ['error', 'warn'],
+    log: ["error", "warn"],
   });
 
-if (process.env.NODE_ENV !== 'production') {
+if (process.env.NODE_ENV !== "production") {
   globalForPrisma.prisma = prisma;
 }
 
